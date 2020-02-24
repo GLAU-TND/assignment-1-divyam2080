de7de9ae4c356a012ad2e9f4767504b1404cb7b4
divyam guptta <divyam.gupta_cs18@gla.ac.in>
2020-02-24 22:34:39 +0530

    cases for the choices

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        LinkedList linkedList = new LinkedList();
+        Functions functions = new Functions();
+
+        System.out.println("hey welcome peeps , here is divyam , how can i help you!!!!!!!!!!!");
+        int c=0;
+        while(c!=5) {
+            System.out.println("<----------x------x---------->");
+            System.out.println("Press 1 to add a new contact");
+            System.out.println("Press 2 to view all contacts");
+            System.out.println("Press 3 to search for a contact");
+            System.out.println("Press 4 to delete a contact");
+            System.out.println("Press 5 to exit program ");
+            System.out.println("<-----------x------x---------->");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    functions.addContact(linkedList);
+                    break;
+                case "2":functions.showContacts(linkedList);
+                    break;
+                case "3":functions.searchContact(linkedList);
+                    break;
+                case "4":functions.deleteContact(linkedList);
+                    break;
+                case "5":
+                    c=5;
+                    System.out.println("Thanks");
+                    break;
+                default:
+                    System.out.println("Invalid ");
+            }
+        }
+
+    }
+}
+
